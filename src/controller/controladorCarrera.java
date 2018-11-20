73d98b0e759974967be7fca62850a3ac57526435
Leonel Gonzalez Vidales <leonelgv@gmail.com>
2018-11-20 07:34:44 -0600

    Modelos y controladores del CRUD implementados.

--- src/controller/controladorCarrera.java
+++ src/controller/controladorCarrera.java
@@ -3,6 +3,8 @@ package controller;
 import include.Carrera;
 import model.modeloCarrera;
 
+import java.util.ArrayList;
+
 public class controladorCarrera {
 
     public boolean agregarCarrera(Carrera carrera){
@@ -10,9 +12,59 @@ public class controladorCarrera {
         return carrera1.insertarCarrera(carrera);
     }
 
+    public ArrayList<Carrera> obtenerCarreras(){
+        modeloCarrera carrera = new modeloCarrera();
+        return carrera.obtenerCarreras();
+    }
+
+    public ArrayList<Carrera> obtenerCarrera(String clave){
+        modeloCarrera carrera = new modeloCarrera();
+        return carrera.obtenerCarrera(clave);
+    }
+    public boolean actualizarCarrera(Carrera carrera){
+        modeloCarrera carrera1 = new modeloCarrera();
+        return carrera1.actualizarCarrera(carrera);
+    }
+
+    public boolean eliminarCarrera(String clave){
+        modeloCarrera carrera1 = new modeloCarrera();
+        return carrera1.borrarCarrera(clave);
+    }
+
     public static void main(String[] args){
+
         controladorCarrera cc = new controladorCarrera();
-        System.out.println(cc.agregarCarrera(new Carrera("Informatica", "6", "Nivel superior", "IINF-2010-220", "Ingeniería Informática", "IINF", 32, 260, "1", "Escolarizada")));
+        //Agregar un elemento
+        //System.out.println(cc.agregarCarrera(new Carrera("Biologia", "8", "Nivel superior", "LBIO-2010-220", "Licenciatura en Biología", "LBIO", 32, 260, "1", "Escolarizada")));
+
+        //Listar todos los elementos de la tabla
+        ArrayList<Carrera> carreras = new ArrayList<Carrera>();
+        carreras = cc.obtenerCarreras();
+        for (int a=0;a<carreras.size();a++){
+            System.out.println("Carrera: " + carreras.get(a).getCarrera());
+            System.out.println("Reticula: "+ carreras.get(a).getNombre_carrera());
+            System.out.println("Nivel escolar: "+ carreras.get(a).getNivel_escolar());
+            System.out.println("Clave: "+ carreras.get(a).getClave_oficial());
+            System.out.println();
+        }
+
+        //Listar un elemento de la tabla por su llave primaria
+        ArrayList<Carrera> carrera = new ArrayList<Carrera>();
+        carrera = cc.obtenerCarrera("IINF-2010-220");
+
+        for (int b=0;b<carrera.size();b++){
+            System.out.println("Carrera: " + carrera.get(b).getCarrera());
+            System.out.println("Reticula: "+ carrera.get(b).getNombre_carrera());
+            System.out.println("Nivel escolar: "+ carrera.get(b).getNivel_escolar());
+            System.out.println("Clave: "+ carrera.get(b).getClave_oficial());
+            System.out.println();
+        }
+
+        //Modificar un elemento
+        System.out.println(cc.actualizarCarrera(new Carrera("Biologia", "7", "Licenciatura", "LBIO-2010-220", "Licenciatura en Biología", "LBIO", 32, 260, "1", "Escolarizada")));
+
+        //Modificar un elemento
+        System.out.println(cc.eliminarCarrera("LBIO-2010-220"));
     }
 
 }
--- src/model/modeloCarrera.java
+++ src/model/modeloCarrera.java
@@ -3,6 +3,8 @@ package model;
 import include.Carrera;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class modeloCarrera extends conexion {
 
@@ -30,6 +32,7 @@ public class modeloCarrera extends conexion {
             if(pst.executeUpdate() == 1){
                 flag = true;
             }
+
         } catch (Exception ex){
             System.out.println(ex.getMessage());
         } finally {
@@ -41,6 +44,143 @@ public class modeloCarrera extends conexion {
             }
         }
         return flag;
+        }
+
+    public ArrayList<Carrera> obtenerCarreras(){
+        ArrayList<Carrera> carreras = new ArrayList<Carrera>();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            String consulta = "SELECT CARRERA, RETICULA, NIVEL_ESCOLAR, " +
+                    "CLAVE_OFICIAL, NOMBRE_CARRERA, NOMBRE_REDUCIDO, " +
+                    "CARGA_MAXIMA, CREDITOS_TOTALES, NIVEL, MODALIDAD " +
+                    "FROM CARRERA ";
+            pst = getConection().prepareCall(consulta);
+            rs = pst.executeQuery();
+            while (rs.next()){
+                carreras.add(new Carrera(rs.getString("CARRERA"),
+                        rs.getString("RETICULA"),
+                        rs.getString("NIVEL_ESCOLAR"),
+                        rs.getString("CLAVE_OFICIAL"),
+                        rs.getString("NOMBRE_CARRERA"),
+                        rs.getString("NOMBRE_REDUCIDO"),
+                        rs.getInt("CARGA_MAXIMA"),
+                        rs.getInt("CREDITOS_TOTALES"),
+                        rs.getString("NIVEL"),
+                        rs.getString("MODALIDAD")));
+            }
+        } catch (Exception e) {
+
+        }finally {
+            try {
+                if(getConection() != null)getConection().close();
+                if(pst != null)pst.close();
+                if(rs !=null)rs.close();
+            } catch (Exception e) {
+
+            }
+        }
+        return carreras;
     }
 
+    public ArrayList<Carrera> obtenerCarrera(String clave){
+        ArrayList<Carrera> carrera = new ArrayList<Carrera>();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        try {
+            String consulta = "SELECT CARRERA, RETICULA, NIVEL_ESCOLAR, " +
+                    "CLAVE_OFICIAL, NOMBRE_CARRERA, NOMBRE_REDUCIDO, " +
+                    "CARGA_MAXIMA, CREDITOS_TOTALES, NIVEL, MODALIDAD " +
+                    "FROM CARRERA WHERE CLAVE_OFICIAL = ?";
+            pst = getConection().prepareCall(consulta);
+            pst.setString(1, clave);
+            rs = pst.executeQuery();
+            while (rs.next()){
+                carrera.add(new Carrera(rs.getString("CARRERA"),
+                        rs.getString("RETICULA"),
+                        rs.getString("NIVEL_ESCOLAR"),
+                        rs.getString("CLAVE_OFICIAL"),
+                        rs.getString("NOMBRE_CARRERA"),
+                        rs.getString("NOMBRE_REDUCIDO"),
+                        rs.getInt("CARGA_MAXIMA"),
+                        rs.getInt("CREDITOS_TOTALES"),
+                        rs.getString("NIVEL"),
+                        rs.getString("MODALIDAD")));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally {
+            try {
+                if(getConection() != null)getConection().close();
+                if(pst != null)pst.close();
+                if(rs !=null)rs.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return carrera;
+    }
+
+    public boolean actualizarCarrera(Carrera carrera){
+        PreparedStatement pst = null;
+        boolean flag = false;
+        try{
+            String consulta = "UPDATE CARRERA SET CARRERA = ?, RETICULA = ?, " +
+                    "NIVEL_ESCOLAR = ?, CLAVE_OFICIAL = ?, NOMBRE_CARRERA = ?, " +
+                    "NOMBRE_REDUCIDO = ?, CARGA_MAXIMA = ?, CREDITOS_TOTALES = ?, " +
+                    "NIVEL = ?, MODALIDAD = ? WHERE CLAVE_OFICIAL = ?";
+            pst = getConection().prepareStatement(consulta);
+            pst.setString(1,carrera.getCarrera());
+            pst.setString(2,carrera.getReticula());
+            pst.setString(3,carrera.getNivel_escolar());
+            pst.setString(4, carrera.getClave_oficial());
+            pst.setString(5, carrera.getNombre_carrera());
+            pst.setString(6, carrera.getNombre_reducido());
+            pst.setInt(7, carrera.getCarga_maxima());
+            pst.setInt(8, carrera.getCreditos_totales());
+            pst.setString(9, carrera.getNivel());
+            pst.setString(10, carrera.getModalidad());
+            pst.setString(11, carrera.getClave_oficial());
+
+            if(pst.executeUpdate() == 1){
+                flag = true;
+            }
+
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (getConection() != null) getConection().close();
+                if (pst != null) pst.close();
+            } catch (Exception a) {
+                System.out.println(a.getMessage());
+            }
+        }
+        return flag;
+    }
+
+    public boolean borrarCarrera(String clave){
+        PreparedStatement pst = null;
+        boolean flag = false;
+        try{
+            String consulta = "DELETE FROM CARRERA WHERE CLAVE_OFICIAL = ?";
+            pst = getConection().prepareStatement(consulta);
+            pst.setString(1, clave);
+
+
+            if(pst.executeUpdate() == 1){
+                flag = true;
+            }
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+        } finally {
+            try {
+                if (getConection() != null) getConection().close();
+                if (pst != null) pst.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return flag;
+    }
 }
